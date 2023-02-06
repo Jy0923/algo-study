8685f4c799b955c76e016c3b294d4060dd4eeb25
Yoonoi <dbddbd39@naver.com>
2023-02-06 22:41:40 +0900

    [+] Add 01타일 윤영

--- 0x10-dp/Yy_1904.java
+++ 0x10-dp/Yy_1904.java
@@ -1,20 +1,19 @@
-package silver.s3;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class No1904 {
+public class Main {
+	
+	static int[] arr;
 	
 	public static void fibonacci(int k) {
-		// ↓ 얘를 arr = new int[100000]; 이렇게 하면 맞는데 아래처럼 하면 자꾸 틀렸다고 나오는데 뭔차인지 아시는분 ㅠㅠ....
-		int[] arr = new int[k+1];
+		arr = new int[k+1];
 		
 		if(k <= 2) {
-			for(int i=0; i<=k; i++) {
-				arr[i] = i;
-			}
-			
+            		for(int i=0; i<=k; i++){
+                			arr[i] = i;
+            		}
+            
 			System.out.println(arr[k]);
 			return;
 		}
@@ -23,10 +22,10 @@ public class No1904 {
 		arr[2] = 2;
 		
 		for(int i=3; i<=k; i++) {
-			arr[i] = arr[i-1] + arr[i-2];
+			arr[i] = (arr[i-1] + arr[i-2]) % 15746;
 		}
 		
-		System.out.println(arr[k] % 15746);
+		System.out.println(arr[k]);
 	}
 	
 
