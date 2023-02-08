62bceaf3adb887151572b10b2e22915bfef30a59
Joslynn <joslynnkim@gmail.com>
2023-02-09 01:19:38 +0900

    [+] Add 신나는 함수 실행 보경

---
+++ 0x10-dp/Bk_9148.java
@@ -0,0 +1,58 @@
+package dp;
+
+import java.util.Scanner;
+
+public class Bk_9148 {
+
+	static int[][][] dp = new int[21][21][21];
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		StringBuilder sb = new StringBuilder();
+
+		while (true) {
+
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+
+			// -1 -1 -1 이 나오면 종료
+			if (a == -1 && b == -1 && c == -1) {
+				break;
+			}
+
+			sb.append("w(" + a + ", " + b + ", " + c + ") = ").append(w(a, b, c)).append('\n');
+		}
+		
+		System.out.println(sb);
+
+	}
+
+	static int w(int a, int b, int c) {
+
+		// 이미 저장된 값의 경우 해당 값을 바로 반환
+		if (inRange(a, b, c) && dp[a][b][c] != 0) {
+			return dp[a][b][c];
+		}
+
+		if (a <= 0 || b <= 0 || c <= 0) {
+			return 1;
+		}
+
+		if (a > 20 || b > 20 || c > 20) {
+			return dp[20][20][20] = w(20, 20, 20);
+		}
+
+		if (a < b && b < c) {
+			return dp[a][b][c] = w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
+		}
+
+		return dp[a][b][c] =  w(a - 1, b, c) + w(a - 1, b - 1, c) + w(a - 1, b, c - 1) - w(a - 1, b - 1, c - 1);
+	}
+
+	static boolean inRange(int a, int b, int c) {
+		return 0 <= a && a <= 20 && 0 <= b && b <= 20 && 0 <= c && c <= 20;
+	}
+
+}
